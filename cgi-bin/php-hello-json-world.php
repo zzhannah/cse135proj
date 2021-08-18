7a0baed51fe6a5ac37f61bb1185247dee6c0f865
Sam <z3li@ucsd.edu>
2021-08-17 18:14:21 -0700

    ad

--- cgi-bin/php-hello-json-world.php
+++ cgi-bin/php-hello-json-world.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html');
 header('Cache-Control: no-cache');
 ?>
 <?php
-$time = date("l m/d H:i:sa Y");
+$time = date("l m-d H:i:sa Y");
 $addr = getenv("REMOTE_ADDR");
 $arr = array('title' => 'Hello, Php!', 'message' => 'This page was generated with the Php programming language', 'heading' => 'Hello, Php!', 'time' => $time, 'IP' => $addr);
 
