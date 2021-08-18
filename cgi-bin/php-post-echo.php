93102e5f839a50434aa3dfd909c993c7f82f949c
Sam <z3li@ucsd.edu>
2021-08-18 00:28:22 -0700

    php-general-request-echo

--- cgi-bin/php-post-echo.php
+++ cgi-bin/php-post-echo.php
@@ -10,6 +10,7 @@ header('Cache-Control: no-cache');
     <body>
         <h1 align="center">General Request Echo</h1>
         <hr/>
+        <p><b>Message Body:</b> </p>
         <ul>
             <?php
             foreach($_POST as $key_name => $key_value){
