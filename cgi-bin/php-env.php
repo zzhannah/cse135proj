a6f286a1cc006d3626100384eac7a7ae43ae266e
Sam <z3li@ucsd.edu>
2021-08-17 23:17:25 -0700

    php-env

--- cgi-bin/php-env.php
+++ cgi-bin/php-env.php
@@ -11,15 +11,15 @@ header('Cache-Control: no-cache');
         <h1 align="center">Environment Variables</h1>
         <hr>
         <?php
-            print "<h4>Environment Variables:</h4>"
+            echo "<h4>Environment Variables:</h4>";
             $env_array = getenv();
             foreach ($env_array as $key=>$value){
                 echo "$key => $value <br />";
             }
-            print "<h4>Server Variables:</h4>"
+            echo "<h4>Server Variables:</h4>";
             foreach($_SERVER as $key_name => $key_value) {
 
-                print $key_name . "=" . $key_value . "<br />";
+                echo $key_name . "=" . $key_value . "<br />";
                 
                 }
         ?>
