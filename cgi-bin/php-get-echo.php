f4bd4fbc0b5c7427b2ca9032f2a49606e4c4e2b3
Sam <z3li@ucsd.edu>
2021-08-18 04:29:58 -0700

    update php-sessions

--- cgi-bin/php-get-echo.php
+++ cgi-bin/php-get-echo.php
@@ -4,6 +4,8 @@ header('Cache-Control: no-cache');
 ?>
 <html>
     <head>
+    <script src="https://cdn.lr-in.com/LogRocket.min.js" crossorigin="anonymous"></script>
+        <script>window.LogRocket && window.LogRocket.init('vif6az/cse135proj');</script>
         <title>GET Request Echo</title>
     </head>
     <body>
--- cgi-bin/php-hello-json-world.php
+++ cgi-bin/php-hello-json-world.php
@@ -2,6 +2,11 @@
 header('Content-Type: text/html');
 header('Cache-Control: no-cache');
 ?>
+<html>
+    <head>
+    <script src="https://cdn.lr-in.com/LogRocket.min.js" crossorigin="anonymous"></script>
+        <script>window.LogRocket && window.LogRocket.init('vif6az/cse135proj');</script>
+    </head>
 <?php
 $time = date("l m-d H:i:sa Y");
 $addr = getenv("REMOTE_ADDR");
