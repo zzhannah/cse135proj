a3cca34187c9dedb45c47cb5f3202b1293f679d0
Sam <z3li@ucsd.edu>
2021-08-17 23:45:35 -0700

    php-get-echo

---
+++ cgi-bin/php-post-echo.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: text/html');
+header('Cache-Control: no-cache');
+?>
+
+<html>
+    <head>
+        <title>POST Request Echo</title>
+    </head>
+    <body>
+        <h1 align=center>POST Request Echo</h1>
+        <hr/>
+        <ul>
+            <?php
+            foreach($_POST as $key_name => $key_value){
+                echo "<p>$key_name = $key_value</p>";
+            }
+            ?>
+        </ul>
+    </body>
+</html>
