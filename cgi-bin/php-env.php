18fdbb0b552a09a9258edabb883328487941e9ef
Sam <z3li@ucsd.edu>
2021-08-17 23:01:42 -0700

    php-env

--- cgi-bin/php-env.php
+++ cgi-bin/php-env.php
@@ -12,20 +12,10 @@ header('Cache-Control: no-cache');
         <hr>
         <?php
             
-$env_array =getenv();
-
-echo "<h3>The list of environment variables with values are :</h3>";
-
-//Print all environment variable names with values
-
-foreach ($env_array as $key=>$value)
-
-{
-
-    echo "$key => $value <br />";
-
-}
-
+            $env_array =getenv();
+            foreach ($env_array){
+                echo "$key => $value <br />";
+            }
         ?>
     </body>    
 </html>
