20af56117b4fa519fb2ec3fed5a7674dab45c68f
Sam <z3li@ucsd.edu>
2021-08-19 01:27:46 -0700

    pp

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -10,9 +10,10 @@ print "<title>POST Message Body</title>"
 print "</head>"
 print "<body>"
 print "<h1>POST Message Body</h1>"
-args = request.body
-print args
-
+args=sys.stdin.read().split('&')
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k, v=arg.split('='); POST[k]=v
 print "<p>Message Body: </p>"
 print "</body>"
 print "</html>"
