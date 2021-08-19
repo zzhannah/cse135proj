deaf8c1873b85211a1bd9053902e190a3d476663
Sam <z3li@ucsd.edu>
2021-08-19 01:04:28 -0700

    pp

--- cgi-bin/py-env.py
+++ cgi-bin/py-env.py
@@ -5,7 +5,7 @@ print "Content-type: text/html"
 print "Cache-Control: no-cache"
 print
 
-print "<HTML><HEAD><TITLE>Environment Variables</TITLE></HEAD><BODY><H1 align="center">Environment Variables</H1><br>"
+print "<HTML><HEAD><TITLE>Environment Variables</TITLE></HEAD><BODY><H1 align='center'>Environment Variables</H1><br>"
 
 for headername, headervalue in os.environ.iteritems():
    print "<p>{0} = {1}</p>".format(headername, headervalue)
