815deb52cdb34c9f29296d0b91b736da086f2294
Sam <z3li@ucsd.edu>
2021-08-19 06:17:26 -0700

    aa

--- cgi-bin/py-destroy-session.py
+++ cgi-bin/py-destroy-session.py
@@ -21,19 +21,23 @@ for arg in args:
 
 # r = requests.get(setCookieUrl, auth=auth)
 # print(r.content)
+if POST['name']:
+    print ("<p><b>Name: </b>"+ POST['name'] + "</p>")
+else:
+    print ("<p><b>Name: </b>you do not have a name set</p>")
 
 
+print ("<a href='/py-cgiform.html'>CGI Form</a><br>")
 
-# print ("<a href='/py-cgiform.html'>CGI Form</a><br>")
+print ("<form action='/cgi-bin/py-sessions-1.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Session 1</button></form>")
+# print ("<a href='/cgi-bin/py-sessions-1.py'>Session Page 1</a><br>")
+# print ("<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>")
 
-# print ("<form action='/cgi-bin/py-sessions-1.py' method='post'> id=form1'")
-# print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
-# print ("<button type='submit'>Session 1</button></form>")
-
-# print ("<form action='/cgi-bin/py-sessions-2.py' method='post'> id='form'")
-# print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
-# print ("<button type='submit'>Session 2</button></form>")
-
+print ("<form action='/cgi-bin/py-destroy-session.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Destroy Session</button></form>")
 
 print ("</body>")
 print ("</html>")
