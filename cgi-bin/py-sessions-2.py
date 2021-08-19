be7787ccb74b715413ba210184eeb847733a8d55
Sam <z3li@ucsd.edu>
2021-08-19 03:27:03 -0700

    ss

--- cgi-bin/py-sessions-2.py
+++ cgi-bin/py-sessions-2.py
@@ -19,7 +19,7 @@ print args.body()
 
 print "<p><b>Name: </b>", v, "</p>"
 
-print "<a href='/py-cgiform.html>CGI Form</a><br />"
+print "<a href='/py-cgiform.html'>CGI Form</a><br />"
 print "<a href='/cgi-bin/py-sessions-1.py'>Session Page 1</a><br />"
 print "<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>"
 print "</body>"
