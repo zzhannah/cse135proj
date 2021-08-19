5f95951544f7e53ecaf3be794067aa4e6ee0963c
Sam <z3li@ucsd.edu>
2021-08-19 04:39:37 -0700

    ss

--- cgi-bin/py-sessions-1.py
+++ cgi-bin/py-sessions-1.py
@@ -22,21 +22,23 @@ for arg in args:
     t=arg.split('=')
     if len(t)>1: k, v=arg.split('='); POST[k]=v
 
-if POST['name']:
-    s.post(setCookieUrl, params={'name': POST['name']}, auth=auth)
-r = requests.get(setCookieUrl, auth=auth)
-print(r.content)
+# if POST['name']:
+#     s.post(setCookieUrl, params={'name': POST['name']}, auth=auth)
+
+# r = requests.get(setCookieUrl, auth=auth)
+# print(r.content)
 if POST['name']:
     namme = POST['name']
 else:
     namme = "You do not have a name set"
-
+print "<form action=/cgi-bin/py-sessions-2.py' method='Post' id='form'>"
+print "<input type='hidden' name='name' value='", namme, "'/>"
+print "</form>"
 print "<p><b>Name: </b>", namme, "</p>"
 
 print "<a href='/py-cgiform.html'>CGI Form</a><br>"
 print "<a href='/cgi-bin/py-sessions-2.py'>Session Page 2</a><br>"
 print "<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>"
-print "</form>"
 print "</body>"
 print "</html>"
 
--- cgi-bin/py-sessions-2.py
+++ cgi-bin/py-sessions-2.py
@@ -13,13 +13,27 @@ print "<title>Python Session Page 2</title>"
 print "</head>"
 print "<body>"
 print "<h1 align='center'>Python Session Page 2</h1><hr>"
-args = s.get(setCookieUrl)
-print args.text
+POST={}
+args=sys.stdin.read().split('&')
 
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k, v=arg.split('='); POST[k]=v
 
+# if POST['name']:
+#     s.post(setCookieUrl, params={'name': POST['name']}, auth=auth)
 
-print "<p><b>Name: </b>", v, "</p>"
+# r = requests.get(setCookieUrl, auth=auth)
+# print(r.content)
+if POST['name']:
+    namme = POST['name']
+else:
+    namme = "You do not have a name set"
 
+print "<form action=/cgi-bin/py-sessions-1.py' method='Post' id='form'>"
+print "<input type='hidden' name='name' value='", namme, "'/>"
+print "</form>"
+print "<p><b>Name: </b>", namme, "</p>"
 print "<a href='/py-cgiform.html'>CGI Form</a><br>"
 print "<a href='/cgi-bin/py-sessions-1.py'>Session Page 1</a><br>"
 print "<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>"
