979e340744431c973ba487846f2b337511eccc10
Sam <z3li@ucsd.edu>
2021-08-19 01:21:33 -0700

    pp

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Import modules for CGI handling 
-import cgi, cgitb, os, request
+import cgi, cgitb, os, requests
 
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
@@ -10,7 +10,7 @@ print "<title>POST Message Body</title>"
 print "</head>"
 print "<body>"
 print "<h1>POST Message Body</h1>"
-args = requests.body
+args = request.body
 print args
 
 print "<p>Message Body: </p>"
