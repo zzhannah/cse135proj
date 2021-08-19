57b80a75cfe1e4073361823904cf68df77f0d3a9
Sam <z3li@ucsd.edu>
2021-08-19 03:39:17 -0700

    ss

--- cgi-bin/py-sessions-1.py
+++ cgi-bin/py-sessions-1.py
@@ -4,7 +4,7 @@
 import cgi, cgitb, os, sys
 from flask import Flask, session, redirect, url_for, escape, request
 app = Flask(__name__)
-app.secret_key = 'hh’
+app.secret_key = 'hh'
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
