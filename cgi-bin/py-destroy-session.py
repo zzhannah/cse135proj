6f084025429720051aa8e945382b082433cd68d5
Sam <z3li@ucsd.edu>
2021-08-19 06:11:56 -0700

    aa

--- cgi-bin/py-destroy-session.py
+++ cgi-bin/py-destroy-session.py
@@ -1,33 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 # Import modules for CGI handling 
-import os, sys
+import cgi, cgitb, os, sys
 print ("Content-type:text/html\r\n\r\n")
 print ("<html>")
 print ("<head>")
-print ("<title>Session Destroy</title>")
+print ("<title>Python Session Page 2</title>")
 print ("</head>")
 print ("<body>")
-print ("<h1 align='center'>Session Destroyed</h1><hr>")
+print ("<h1 align='center'>Python Session Page 2</h1><hr>")
+POST={}
+args=sys.stdin.read().split('&')
 
-# POST={}
-# args=sys.stdin.read().split('&')
-
-# for arg in args: 
-#     t=arg.split('=')
-#     if len(t)>1: k, v=arg.split('='); POST[k]=v
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k, v=arg.split('='); POST[k]=v
 
 # if POST['name']:
-#    POST['name'] = None
+#     s.post(setCookieUrl, params={'name': POST['name']}, auth=auth)
+
+# r = requests.get(setCookieUrl, auth=auth)
+# print(r.content)
+if POST['name']:
+    print ("<p><b>Name: </b>"+ POST['name'] + "</p>")
+else:
+    print ("<p><b>Name: </b>you do not have a name set</p>")
 
 
-# print ("<form action='/cgi-bin/py-sessions-1.py' method='post'>")
-# print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
-# print ("<button type='submit'>Session 1</button></form>")
-# print ("<form action='/cgi-bin/py-sessions-2.py' method='post'>")
-# print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
-# print ("<button type='submit'>Session 2</button></form>")
-# print ("<a href='/py-cgiform.html'>Python CGI Form</a>")
+print ("<a href='/py-cgiform.html'>CGI Form</a><br>")
+
+print ("<form action='/cgi-bin/py-sessions-1.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Session 1</button></form>")
+# print ("<a href='/cgi-bin/py-sessions-1.py'>Session Page 1</a><br>")
+# print ("<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>")
+
+print ("<form action='/cgi-bin/py-destroy-session.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Destroy Session</button></form>")
 
 print ("</body>")
 print ("</html>")
