6d7f361b29031a5ee0fdaef53b16a49d0934cd7e
Sam <z3li@ucsd.edu>
2021-08-19 01:41:52 -0700

    py-post

--- cgi-bin/py-general-request-echo.py
+++ cgi-bin/py-general-request-echo.py
@@ -10,9 +10,32 @@ print "<title>General Request Echo</title>"
 print "</head>"
 
 print "<body>"
-print "<h1>General Request Echo</h1>" 
-print "<p>Protocol: %s</p>" % cgi.escape(os.environ["SERVER_PROTOCOL"])
-print "<p>Method: %s</p>" % cgi.escape(os.environ["REQUEST_METHOD"])
-print "<p>Message Body: %s</p>" % cgi.escape(os.environ["QUERY_STRING"])
+print "<h1 align='center'>General Request Echo</h1><hr>" 
+print "<p><b>Protocol:</b> %s</p>" % cgi.escape(os.environ["SERVER_PROTOCOL"])
+print "<p><b>Method:</b> %s</p>" % cgi.escape(os.environ["REQUEST_METHOD"])
+
+print "<p><b>Query String:</b> </p>"
+print "<ul>"
+GET={}
+args=os.getenv("QUERY_STRING").split('&')
+
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k,v=arg.split('='); GET[k]=v
+for a in POST:
+    print "<li>", a , "=" , POST[a], "</li>"
+
+print "</ul>"
+print "<p><b>Message Body:</b> </p>"
+print "<ul>"
+POST={}
+args=sys.stdin.read().split('&')
+
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k, v=arg.split('='); POST[k]=v
+for a in POST:
+    print "<li>", a , "=" , POST[a], "</li>"
+print "</ul>"
 print "</body>"
 print "</html>"
