98012d246a0590a13419b61f05df040a125a68d1
Sam <z3li@ucsd.edu>
2021-08-19 03:27:52 -0700

    ss

--- cgi-bin/py-sessions-2.py
+++ cgi-bin/py-sessions-2.py
@@ -13,14 +13,14 @@ print "</head>"
 print "<body>"
 print "<h1 align='center'>Python Session Page 2</h1><hr>"
 args = s.get(getCookieUrl)
-print args.body()
+print args.text
 
 
 
 print "<p><b>Name: </b>", v, "</p>"
 
-print "<a href='/py-cgiform.html'>CGI Form</a><br />"
-print "<a href='/cgi-bin/py-sessions-1.py'>Session Page 1</a><br />"
+print "<a href='/py-cgiform.html'>CGI Form</a><br>"
+print "<a href='/cgi-bin/py-sessions-1.py'>Session Page 1</a><br>"
 print "<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>"
 print "</body>"
 print "</html>"
