15f444cdcdb81b72a5205cd74c582d364aa1b291
Sam <z3li@ucsd.edu>
2021-08-19 06:04:08 -0700

    aa

--- cgi-bin/py-destroy-session.py
+++ cgi-bin/py-destroy-session.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 # Import modules for CGI handling 
-import cgi, cgitb, os 
-
-
+import cgi, cgitb, os, sys
 print ("Content-type:text/html\r\n\r\n")
-
 print ("<html>")
 print ("<head>")
-print ("<title>Python Session Destroyed</title>")
+print ("<title>Session Destroy</title>")
 print ("</head>")
 print ("<body>")
-
+print ("<h1 align='center'>Session Destroyed</h1><hr>")
 POST={}
 args=sys.stdin.read().split('&')
 
@@ -19,14 +16,18 @@ for arg in args:
     t=arg.split('=')
     if len(t)>1: k, v=arg.split('='); POST[k]=v
 
-POST['name'] = None
+if POST['name']:
+   POST['name'] = None
 
-print ("<form action='/cgi-bin/py-sessions-1.py' method='post'>")
-print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
-print ("<button type='submit'>Session 1</button></form>")
-print ("<form action='/cgi-bin/py-sessions-2.py' method='post'>")
-print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
-print ("<button type='submit'>Session 2</button></form>")
-print ("<a href='/py-cgiform.html'>Python CGI Form</a>")
+
+print ("<form action='/cgi-bin/py-sessions-1.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Session 1</button></form>")
+print ("<form action='/cgi-bin/py-sessions-2.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Session 2</button></form>")
+print ("<a href='/py-cgiform.html'>Python CGI Form</a>")
+
 print ("</body>")
 print ("</html>")
+
