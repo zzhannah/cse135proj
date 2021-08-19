eec8e4bd31b2624c8934e0c8241efe0067891090
Sam <z3li@ucsd.edu>
2021-08-19 02:34:49 -0700

    session

---
+++ cgi-bin/py-post-echo copy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling 
+import cgi, cgitb, os, sys
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>POST Message Body</title>"
+print "</head>"
+print "<body>"
+print "<h1 align='center'>POST Message Body</h1><hr>"
+print "<ul>"
+POST={}
+args=sys.stdin.read().split('&')
+
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k, v=arg.split('='); POST[k]=v
+
+for a in POST:
+    print "<li>", a , "=" , POST[a], "</li>"
+
+print "</ul>"
+print "<p><b>Message Body: </b></p>"
+print "</body>"
+print "</html>"
