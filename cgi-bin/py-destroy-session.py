dce8f07a66c9f57b537dc4483e61783ed6c22150
Sam <z3li@ucsd.edu>
2021-08-19 05:57:02 -0700

    aa

--- cgi-bin/py-destroy-session.py
+++ cgi-bin/py-destroy-session.py
@@ -3,27 +3,32 @@
 # Import modules for CGI handling 
 import cgi, cgitb, os 
 
-# Create instance of FieldStorage 
-form = cgi.FieldStorage() 
 
-# Get data from fields
-if form.getvalue('textcontent'):
-   msg = form.getvalue('textcontent')
+print ("Cache-Control: no-cache\n")
+print ("Set-Cookie: destroyed\n")
+print ("Content-type:text/html\n")
 
-print "Cache-Control: no-cache\n"
-print "Set-Cookie: destroyed\n"
-print "Content-type:text/html\n"
+print ("<html>")
+print ("<head>")
+print ("<title>Python Session Destroyed</title>")
+print ("</head>")
+print ("<body>")
 
-print "<html>"
-print "<head>"
-print "<title>Python Session Destroyed</title>"
-print "</head>"
+POST={}
+args=sys.stdin.read().split('&')
 
-print "<body>"
-print "<a href='/cgi-bin/py-sessions-1.py'>Back to Page 1</a>"
-print "<br />" 
-print "<a href='/cgi-bin/py-sessions-2.py'>Back to Page 2</a>"
-print "<br />"
-print "<a href='/py-cgiform.html'>Python CGI Form</a>"
-print "</body>"
-print "</html>"
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k, v=arg.split('='); POST[k]=v
+
+POST['name'] = None
+
+print ("<form action='/cgi-bin/py-sessions-1.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Session 1</button></form>")
+print ("<form action='/cgi-bin/py-sessions-2.py' method='post'>")
+print ("<input type='hidden' name='name' value='"+ POST['name'] + "'>")
+print ("<button type='submit'>Session 2</button></form>")
+print ("<a href='/py-cgiform.html'>Python CGI Form</a>")
+print ("</body>")
+print ("</html>")
