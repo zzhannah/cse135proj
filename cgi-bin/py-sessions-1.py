1263891a68dca6b4f5ca6514032d80a64b0d5cfa
Sam <z3li@ucsd.edu>
2021-08-19 05:39:56 -0700

    ss

--- cgi-bin/py-sessions-1.py
+++ cgi-bin/py-sessions-1.py
@@ -26,18 +26,18 @@ for arg in args:
 if POST['name']:
     print ("<p><b>Name: </b>"+ POST['name'] + "</p>")
 else:
-    print ("<p><b>Name: </b>"+ POST['name'] + "</p>")
-    
-print ("<form action=/cgi-bin/py-sessions-2.py' method='Post' id='form'>")
+    print ("<p><b>Name: </b>You do not have a name set</p>")
+
+print ("<form action=/cgi-bin/py-sessions-2.py' method='get' id='form'>")
 print ("<input type='hidden' name='name' value='"+ namme + "'/>")
 print ("</form>")
-print ("<p><b>Name: </b>You do not have a name set</p>")
+
 
 print ("<a href='/py-cgiform.html'>CGI Form</a><br>")
 print ("<a href='/cgi-bin/py-sessions-2.py'>Session Page 2</a><br>")
 print ("<form style='margin-top:30px' action='/cgi-bin/py-sessions-2.py' method='get'>")
 print ("<input type='hidden' name='name' value='"+ namme + "'>")
-print ("<button type='submit'>Session 2</button></form>")
+print ("<a type='submit'>Session 2</a></form>")
 print ("<a href='/cgi-bin/py-destroy-session.py'>Destroy Session</a>")
 print ("</body>")
 print ("</html>")
