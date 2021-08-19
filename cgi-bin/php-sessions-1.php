5b4f4e0894cb4c10d0fcc5383750b5ef5b6a7813
Sam <z3li@ucsd.edu>
2021-08-19 03:23:10 -0700

    ss

--- cgi-bin/php-sessions-1.php
+++ cgi-bin/php-sessions-1.php
@@ -24,9 +24,10 @@ session_start();
         ?>
         <a href="/php-cgiform.html">CGI Form</a><br />
         <a href="/cgi-bin/php-sessions-2.php">Session Page 2</a><br />
-        <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
+        <a href="/cgi-bin/php-destroy-session.php">Destroy Session</a>
+        <!-- <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
             <button type="submit">Destroy Session</button>
-        </form>
+        </form> -->
     </body>
 </html>
 
--- cgi-bin/php-sessions-2.php
+++ cgi-bin/php-sessions-2.php
@@ -20,8 +20,9 @@ session_start();
         ?>
         <a href="/php-cgiform.html">CGI Form</a><br />
         <a href="/cgi-bin/php-sessions-1.php">Session Page 1</a><br />
-        <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
+        <a href="/cgi-bin/php-destroy-session.php">Destroy Session</a>
+        <!-- <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
             <button type="submit">Destroy Session</button>
-        </form>
+        </form> -->
     </body>
 </html>
