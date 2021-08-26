a4b00b6489b5c1994540c139050b57d54dc9b0dc
Sam <z3li@ucsd.edu>
2021-08-25 18:57:54 -0700

    collector

--- collector.js
+++ collector.js
@@ -302,7 +302,7 @@ setInterval(function postActivity(){
     console.log(content);
   })();
 
-}, 3000);
+}, 30000);
 /**
  * The "initialize" function here begins the collector program by calling all
  * of the necessary methods. Organizing the code this way makes sure that
