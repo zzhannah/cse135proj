55118ba2ef3b01aebb8be1ad90cfc7d5f7fe2121
Sam <z3li@ucsd.edu>
2021-08-26 04:38:46 -0700

    database

--- database.js
+++ database.js
@@ -1,52 +1,52 @@
-// var MongoClient = require('mongodb').MongoClient;
-// var express = require('express');
-// var app = express();
+// // var MongoClient = require('mongodb').MongoClient;
+// // var express = require('express');
+// // var app = express();
 
-// var url = 'mongodb://localhost/data';
-// MongoClient.connect(url, {useNewUrlParser:true});
-// app.get('/static', function(req, res) {
-//     db.collection('users').find({}).toArray(function(err, docs) {
-//     res.send(docs);
+// // var url = 'mongodb://localhost/data';
+// // MongoClient.connect(url, {useNewUrlParser:true});
+// // app.get('/static', function(req, res) {
+// //     db.collection('users').find({}).toArray(function(err, docs) {
+// //     res.send(docs);
+// //     });
+// //   })
+
+// window.addEventListener('DOMContentLoaded', init);
+
+// var static;
+// var performance;
+// function init(){
+//     postPerformance;
+//     postStatic;
+//     document.getElementById('grid1').data = static;
+//     console.log(static);
+//     document.getElementById('grid1').data = performance;
+//     console.log(performance);
+
+// }
+// function postPerformance(){
+
+//     const url = 'https://cse135proj.site/json/performance';
+//     fetch(url, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     }).then(function(response){
+//         performance = JSON.stringify(response);
+//         console.log(response);
 //     });
-//   })
+//   }
 
-window.addEventListener('DOMContentLoaded', init);
-
-var static;
-var performance;
-function init(){
-    postPerformance;
-    postStatic;
-    document.getElementById('grid1').data = json(static);
-    console.log(static);
-    document.getElementById('grid1').data = json(performance);
-    console.log(performance);
-
-}
-function postPerformance(){
-
-    const url = 'https://cse135proj.site/json/performance';
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function(response){
-        performance = JSON.stringify(response);
-        console.log(response);
-    });
-  }
-
-function postStatic() {
-    const url = 'https://cse135proj.site/json/static';
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(function(response){
-      static = JSON.stringify(response);
-      console.log(response);
-    });
+// function postStatic() {
+//     const url = 'https://cse135proj.site/json/static';
+//     fetch(url, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     }).then(function(response){
+//       static = JSON.stringify(response);
+//       console.log(response);
+//     });
       
-  }
+//   }
