17202dbd4233c54066ce19fce96c1704fab75de8
Sam <z3li@ucsd.edu>
2021-08-25 18:53:35 -0700

    collector

--- collector.js
+++ collector.js
@@ -281,7 +281,7 @@ function bindActivityEvents() {
   });
 }
 
-setTimeout(function postActivity(){
+setInterval(function postActivity(){
   
   (async () => {
     const url = 'https://cse135proj.site/json/api/activity';
@@ -326,7 +326,7 @@ function init() {
 // some time for things to load
 window.addEventListener('DOMContentLoaded', init);
 window.addEventListener('load', postStatic);
-window.addEventListener('load', postActivity);
+//window.addEventListener('load', postActivity);
 window.addEventListener('unload', postPerformance);
 
 
