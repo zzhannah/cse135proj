23c132ef6aecf9d7f2e4e04a5ef0e6baaecb2ad5
Sam <z3li@ucsd.edu>
2021-08-26 06:55:35 -0700

    database

--- routes/statics.js
+++ routes/statics.js
@@ -21,7 +21,7 @@ router.get('/:id', async(req, res) => {
 })
 
 router.post('/', async(req,res) => {
-    console.log("id = ", req.body._idtoSting())
+    console.log("id = ", req.body._id.toSting())
     const static = new Static({
         id:req.body._id.toSting(),
         userAgent: req.body.userAgent,
