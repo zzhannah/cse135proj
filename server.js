1db22e59ac83e5ecfb1ba5c8a2e92b45d0726451
Sam <z3li@ucsd.edu>
2021-09-01 10:57:48 -0700

    ss

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   }
-  res.redirect('/login')
+  res.redirect('/api/login')
 }
 
 function checkNotAuthenticated(req, res, next) {
